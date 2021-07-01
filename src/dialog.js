231519cf8891c23072c7a77ec4a88eb4a6cc5a06
frank-deng <13382084679@163.com>
2021-07-01 18:14:04 +0800

    Updated dialog.js

--- src/dialog.js
+++ src/dialog.js
@@ -2,10 +2,8 @@ import {Component} from 'react';
 import ReactDOM from 'react-dom';
 
 export default class Dialog extends Component{
+  //Modal容器对应的DOM
   container=null;
-  constructor(){
-    super();
-  }
   componentDidUpdate(){
     let visible=this.props.visible;
     //切换body上的容器的新建和销毁
@@ -13,14 +11,13 @@ export default class Dialog extends Component{
       document.body.removeChild(this.container);
       this.container=null;
     }else if(visible && !this.container){
-      let body = document.body;
       let container = document.createElement("div");
       container.classList.add('dialog-container');
       if(this.props.modalClass){
         container.classList.add(this.props.modalClass);
       }
       this.container=container;
-      body.appendChild(container);
+      document.body.appendChild(container);
     }
     //更新对话框里的内容
     if(!visible){
@@ -38,11 +35,13 @@ export default class Dialog extends Component{
     );
   }
   componentWillUnmount(){
+    //Modal被销毁时，将对应的DOM手动清除
     if(this.container){
       document.body.removeChild(this.container);
       this.container=null;
     }
   }
+  //不使用组件默认的render()函数，因为该组件对应的DOM不在父组件对应的DOM下面，而在body上
   render(){
     return null;
   }
